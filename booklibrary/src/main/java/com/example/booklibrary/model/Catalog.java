fd590c53ef5fe92b9f7ea898ede820c51eae67e7
lottydd <daniiilrybak139@gmail.com>
2025-04-23 13:58:46 +0600

    Adjusted model module with jpa

--- booklibrary/src/main/java/com/example/booklibrary/model/Catalog.java
+++ booklibrary/src/main/java/com/example/booklibrary/model/Catalog.java
@@ -1,4 +1,32 @@
 package com.example.booklibrary.model;
 
-public class Catalog {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.catalog.Catalog;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Catalogs {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Catalog parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Catalog> children;
+
+    @ManyToMany(mappedBy = "catalogs")
+    private List<Book> books;
+
 }
